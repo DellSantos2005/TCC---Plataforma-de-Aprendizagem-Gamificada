4beb94bd2849d5b06cec237713844309551e7320
DellSantos2005 <eliedsomsilva09@gmail.com>
2025-12-13 12:04:15 -0300

    Update login.js

--- Scripts/login.js
+++ Scripts/login.js
@@ -21,8 +21,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     alert("Login realizado com sucesso!");
-    window.location.href = "/pages/Principal/Home.html";
+    window.location.href = "Principal/Home.html";
   });
 });
+
 
 
